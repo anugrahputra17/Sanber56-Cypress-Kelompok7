6c4eb05d1d70018f0f5d49d5b4bac0a14304398c
anugrahputra17 <muhammadanugrahp@gmail.com>
2024-05-17 16:50:51 +0700

    add fixture to spec

--- cypress/e2e/login/loginspec.spec.cy.js
+++ cypress/e2e/login/loginspec.spec.cy.js
@@ -3,8 +3,12 @@ import loginPage from "../../support/page-objects/loginPage"
 describe('Login using POM', () => {
   it('Success Login', () => {
     loginPage.visit()
-    loginPage.fillEmail('nama_pengguna')
-    loginPage.fillPassword('kata_sandi')
+    cy.fixture('users.json').then((users) => {
+      const datauser = users[0];
+    loginPage.fillEmail(datauser.email)
+    loginPage.fillPassword(datauser.password)
     loginPage.clickLoginButton()
+    });
   })
 })
+
